d678fb1c3e02b6c69f82f8751f985ff6c4855ce3
Bibhu <bibhushitregmi0@gmail.com>
2024-10-16 17:52:45 -0400

    Modified to handle the utility function and added Schedule screen to navigation stack

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';  // Corrected import
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
 import ForgotPasswordScreen from './screens/ForgetPassword';
+import ScheduleScreen from './screens/ScheduleScreen';
+import { addpredefinedEvents } from './utilities/addpredefinedEvents';
+
+const App = () => {
+  // useEffect (() => {
+  //   const initializePredefinedEvents = async () => {
+  //     await addpredefinedEvents();
+  //   };
+  //   initializePredefinedEvents();
+  // }, []); this function add the data provided in the utilities folder to the firebase firestore
+
 
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
   return (
     <NavigationContainer> 
       <Stack.Navigator initialRouteName="Login">
@@ -27,7 +37,15 @@ export default function App() {
          component={ForgotPasswordScreen}
          options={{headerShown: false}}
         />
+
+        <Stack.Screen
+          name = "ScheduleScreen"
+          component={ScheduleScreen}
+          options={{headerShown: false}}
+          />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+export default App; 
