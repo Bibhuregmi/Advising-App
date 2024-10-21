54214dfb9184fb06d12e2b23a71db1f3c3bfdb3d
Bibhu <bibhushitregmi0@gmail.com>
2024-10-20 21:03:50 -0400

    Implemented persistant login logic and cache to store user credential

--- App.js
+++ App.js
@@ -1,51 +1,72 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';  // Corrected import
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import SignupScreen from './screens/SignupScreen';
-import ForgotPasswordScreen from './screens/ForgetPassword';
-import ScheduleScreen from './screens/ScheduleScreen';
+import { auth } from './firebase'; //firebase setup
+import AppNavigator from './navigation/appNavigator'; //navigator component
+import { onAuthStateChanged } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { addpredefinedEvents } from './utilities/addpredefinedEvents';
+import { ActivityIndicator, View } from 'react-native';
+import { fetchUserDataFromFirestore, storeUserData } from './utilities/userData';
 
 const App = () => {
-  // useEffect (() => {
-  //   const initializePredefinedEvents = async () => {
-  //     await addpredefinedEvents();
-  //   };
-  //   initializePredefinedEvents();
-  // }, []); this function add the data provided in the utilities folder to the firebase firestore
 
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState(null); 
 
+  useEffect (() => {
+    const initializePredefinedEvents = async () => {
+      try{
+        const eventsAdded = await AsyncStorage.getItem('eventsAdded');
+        if (!eventsAdded){
+          await addpredefinedEvents();
+          await AsyncStorage.setItem('eventsAdded', 'true'); //setting flag once the event has been added so that same event won't be added eveytime the app runs
+        }
+      } catch(error) {
+        console.error('Error adding predefined events:', error);
+      }
+    };
+    const setupAuthListner = () => {
+      const unsubscribe = onAuthStateChanged(auth, async (authUser) => {
+        if (authUser){
+          const userData = await fetchUserDataFromFirestore(authUser.uid);
+          const userProfile = {
+            firstName: userData.firstName || 'User',
+            latName: userData.latNameName || 'User',
+            profilePicture: userData.photoURL || null, 
+            course: userData.course,
+          };
+          await storeUserData(userProfile);
+          setUserData(userProfile);
+          setUser(authUser);
+        }else{
+          setUser(null);
+        }
+        setLoading(false);
+      });
+      return unsubscribe;
+    }
+    initializePredefinedEvents(); //call if only the events have not been added before
+    const unsubscribeAuth = setupAuthListner();
+    return () => unsubscribeAuth();
+  }, []); 
 
-const Stack = createNativeStackNavigator();
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
 
+  
   return (
-    <NavigationContainer> 
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name = "Signup"
-          component={SignupScreen}
-          options={{headerShown: false}}
-          />
-        <Stack.Screen
-         name = "ForgetPassword"
-         component={ForgotPasswordScreen}
-         options={{headerShown: false}}
-        />
-
-        <Stack.Screen
-          name = "ScheduleScreen"
-          component={ScheduleScreen}
-          options={{headerShown: false}}
-          />
-      </Stack.Navigator>
+    <NavigationContainer>
+      <AppNavigator user={user} userData={userData}/>
     </NavigationContainer>
   );
 };
+
+
 
 export default App; 
